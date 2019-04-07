b7689c7d025e9ad5600508706147039865f3d782
YuriLukashenko <yuri.lukashenko@computools.com>
2019-04-08 01:15:42 +0300

    Create Profile Controller, AppUser, Upload services"

---
+++ ClassLibrary1.Data/IApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary1.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Data
+{
+    public interface IApplicationUser
+    {
+        ApplicationUser GetById(string id);
+        IEnumerable<ApplicationUser> GetAll();
+        Task SetProfileImage(string id, Uri uri);
+        Task IncrementRating(string id, Type type);
+    }
+}
---
+++ WebApplication1/Controllers/ProfileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary1.Data;
+using ClassLibrary1.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IApplicationUser _userService;
+        private readonly IUpload _uploadService;
+ 
+        public ProfileController(
+            UserManager<ApplicationUser> userManager,
+            IApplicationUser userService, 
+            IUpload uploadService)
+        {
+            _userManager = userManager;
+            _userService = userService;
+            _uploadService = uploadService;
+        }
+
+        public IActionResult Detail(string id)
+        {
+            var model = new ProfileModel
+            {
+                
+            };
+            return View(model);
+        }
+    }
+}
