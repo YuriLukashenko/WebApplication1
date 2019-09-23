39ad266d69f3a627f32394a0996190b966780f89
Yuri Lukashenko <yuri.lukashenko@computools.com>
2019-09-23 20:05:47 +0300

    List of user profiles, styling improvements

--- WebApplication1/Controllers/ProfileController.cs
+++ WebApplication1/Controllers/ProfileController.cs
@@ -64,5 +64,26 @@ namespace WebApplication1.Controllers
        
 
         //}
+
+        public IActionResult Index()
+        {
+            var profiles = _userService.GetAll()
+                .OrderByDescending(user => user.Rating)
+                .Select(u => new ProfileModel
+                {
+                    Email = u.Email,
+                    UserName = u.UserName,
+                    ProfileImageUrl = u.ProfileImageUrl,
+                    UserRating = u.Rating.ToString(),
+                    MemberSince = u.MemberSince
+                });
+
+            var model = new ProfileListModel
+            {
+                Profiles = profiles
+            };
+
+            return View(model);
+        }
     }
 }
